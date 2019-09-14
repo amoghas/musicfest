70bc01f6b5505afee958b8c1bd38fb8e234a1af7
SrinivasMaka <srinivasmaka@gmail.com>
2019-09-14 15:50:02 +0530

    adding large image

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -33,11 +33,11 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   #Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [50,250]
+    process :resize_to_fit => [250,250]
   end
 
   version :large do
-    process :resize_to_fit => [200,800]
+    process :resize_to_fit => [800,800]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
