9e43771efbc403771176e7db6d29c75e9c5e86d1
kamal7605 <kamalakar.kassetti@icentris.com>
2019-09-17 20:23:11 +0530

    UI Changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,48 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+@import 'jquery-3.1.1.min'
+@import 'bootstrap.min'
+@import "slick.min";
+@import "owl.carousel.min";
+@import 'wow'
+
+
+    'use strict';
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+    
+        if (scroll >= 200) {
+            $(".fixed-top").addClass("darkHeader");
+        } else {
+            $(".fixed-top").removeClass("darkHeader");
+        }
+    });
+
+    $('.admission_curosel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 10000,
+        dots: false,
+        nav: true,
+        navText: ["<i class='fa fa-arrow-left''></i>", "<i class='fa fa-arrow-right''></i>"],
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 2
+            },
+            1000: {
+                items: 4
+            },
+            1920: {
+                items: 4
+            }
+        }
+    })	
+    new WOW().init();
