c2bc57d6da070d8283db5de7be4ab84d88f96d9b
SrinivasMaka <srinivasmaka@gmail.com>
2019-09-27 12:34:37 +0530

    devise changes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,16 +1,17 @@
 class Event < ApplicationRecord
-  has_many :event_links
-  has_many :event_photos
+  mount_uploader :file_path, ImageUploader
+  has_many :event_links ,:dependent => :delete_all
+  has_many :event_photos ,:dependent => :delete_all
 
   accepts_nested_attributes_for :event_photos
   accepts_nested_attributes_for :event_links
-  mount_uploader :file_path, ImageUploader
+  
 
   scope :published, -> {
     where(published: true).order("created_at desc")
   }
 
-  scope :editor_choice, ->{
+  scope :editor_choice, -> {
     where(editor_choice: true).order("created_at desc")
   }
 
