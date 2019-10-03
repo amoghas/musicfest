7f38a9a074b81e5e6cb4890c6185dc6dc33d265d
SrinivasMaka <srinivasmaka@gmail.com>
2019-10-03 15:33:45 +0530

    fixed minor issues

--- app/models/event.rb
+++ app/models/event.rb
@@ -37,7 +37,9 @@ class Event < ApplicationRecord
 
 
   def country_name
-    country = ISO3166::Country[self.country]
+    country_code = self.country
+    return nil if country_code.blank?
+    country = ISO3166::Country[country_code]
     country.translations[I18n.locale.to_s] || country.name
   end
 
--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,3 +1,4 @@
 class Theme < ApplicationRecord
   has_many :event
+  validates :tittle,presence: true
 end
